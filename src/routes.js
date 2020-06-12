7ae0f602f92fdf69ea34b1df47ecf937f528122a
PedroUgoline <pedro.ugoline@u4crypto.com>
2020-06-11 22:26:53 -0300

    routes

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,29 @@
-modulo.exports = []
+const Raffle = require('./Raffle');
+modulo.exports = [{
+    method: "GET",
+    path: "/getRaffle",
+    options: {
+      handler: Raffle.getRaffle
+    }
+  },
+  {
+    method: "POST",
+    path: "/doRaffle",
+    options: {
+      handler: Raffle.makeRaffle
+    }
+  },
+  {
+    method: "PUT",
+    path: "/updateRaffle",
+    options: {
+      handler: Raffle.updateRaffle
+    }
+  },
+  {
+    method: "DELETE",
+    path: "/deleteRaffle",
+    options: {
+      handler: Raffle.deleteRaffle
+    }
+  }]
